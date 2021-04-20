607eb3f2e4e3c1b950868cd71239aca0a36ca47c
domynique <dshelbyv@gmail.com>
2021-04-19 22:48:58 -0400

    new

---
+++ app.js
@@ -0,0 +1,34 @@
+// 1. 
+var myArray = [];
+
+
+for (var i=1; i<11; i++){
+  myArray.push(i);
+}
+
+
+//2. 
+var myArray = [];
+
+
+var i = 10;
+while(i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// 3. 
+var myArray = [];
+
+
+for (var i = 11; i <= 21; i += 2) {
+  myArray.push(i);
+}
+
+// 4. 
+var myArray = [];
+
+
+for (var i = 15; i > 0; i -= 2)  {
+  myArray.push(i);
+}
